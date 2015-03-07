9568ae5d1f04bca61f57eab73e752492d24018a5
Lancer He <lancer.he@gmail.com>
2015-03-07 14:30:56 +0800

    增加MySQL Proxy监听数据库表结构进行变更时候记录变更信息的lua脚本

---
+++ lua/mysql-proxy-listen-table-change.lua
@@ -0,0 +1,66 @@
+function read_query( packet )
+        if packet:byte() == proxy.COM_QUERY then
+                if is_alter(packet) or is_create(packet) or is_drop(packet) then
+                        proxy.queries:append(1, packet)
+                        if  'Test_' == get_dbname(packet):sub(1,5) then
+                                return proxy.PROXY_SEND_QUERY
+                        end
+                        proxy.queries:append(2, string.char(proxy.COM_INIT_DB) .. 'notify', {resultset_is_needed = true} )
+
+                        proxy.queries:append(2, string.char(proxy.COM_QUERY) .. "INSERT INTO `notify` (`db`, `query`, `datetime`) VALUES('".. get_dbname(packet) .."','".. get_query(packet) .."', '" .. get_datetime() .. "')", {resultset_is_needed = true} )
+                        proxy.queries:append(2, string.char(proxy.COM_INIT_DB) .. get_dbname(packet), {resultset_is_needed = true} )
+
+                        return proxy.PROXY_SEND_QUERY
+                end
+        end
+end
+
+function read_query_result(inj)
+        if inj.id == 2 then
+                return proxy.PROXY_IGNORE_RESULT
+        end
+
+        if  'Test_' == get_dbname(inj.query):sub(1,5) then
+                return
+        end
+
+        write_log(inj)
+end
+
+function get_datetime()
+        return os.date("%Y-%m-%d %H:%M:%S", os.time())
+end
+
+function is_create(query)
+        return "CREATE" == string.upper(query:sub(2, 7));
+end
+
+function is_drop(query)
+        return "DROP" == string.upper(query:sub(2, 5));
+end
+
+function is_alter(query) 
+        return "ALTER" == string.upper(query:sub(2, 6));
+end
+
+function get_dbname(query)
+        if type( string.match(query, "`(%w+)`%.") ) == 'string' then
+                return string.match(query, "`(%w+)`%.")
+        else
+                return proxy.connection.client.default_db
+        end
+end
+
+function get_query(query)
+        return string.gsub(query:sub(2), "'", '"')
+end
+
+function build_notify(inj)
+        return "Time: \n\t" .. get_datetime() .. "\nDatabase: \n\t" .. get_dbname(inj.query) .. "\nQuery: \n\t" .. get_query(inj.query) .. "\n"
+end
+
+function write_log(inj)
+        local file = io.open("/var/log/mysql-proxy-alter", "a")
+        file:write(build_notify(inj) .. "\n")
+        file:close()
+end
